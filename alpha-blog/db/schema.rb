4cbdd1ab68ec78989d190ca8b06769506a1f431c
atina <atinachang@gmail.com>
2021-09-21 14:00:14 -0400

    add new migrations

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_173849) do
+ActiveRecord::Schema.define(version: 2021_09_21_175702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_09_21_173849) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
