a37dc20197495b7c1c5e47d18d10d2c7ac0ce41a
atina <atinachang@gmail.com>
2021-09-28 14:09:59 -0400

    article views

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -1,3 +1,9 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+Rails
+  .application
+  .routes
+  .draw do
+    root 'pages#home'
+
+    get 'about', to: 'pages#about'
+    resources :articles, only: [:show]
+  end
