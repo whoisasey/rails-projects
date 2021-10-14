65ac6b6c407da101755031baa333045d6a799127
atina <atinachang@gmail.com>
2021-10-14 11:23:13 -0400

    styling show and edit pages

--- alpha-blog/config/environment.rb
+++ alpha-blog/config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Field Error class
+ActionView::Base.field_error_proc =
+  proc { |html_tag, instance| html_tag.html_safe }
---
+++ alpha-blog/config/webpack/environment.js
@@ -0,0 +1,9 @@
+const webpack = require("webpack");
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"],
+  })
+);
