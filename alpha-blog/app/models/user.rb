2432915b20366267daaa10e306021e84ae5af70d
atina <atinachang@gmail.com>
2021-10-25 09:38:37 -0400

    add bcrypt and password digest

--- alpha-blog/app/models/user.rb
+++ alpha-blog/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username,
             presence: true,
@@ -21,4 +22,5 @@ class User < ApplicationRecord
             format: {
               with: VALID_EMAIL_REGEX,
             }
+  has_secure_password
 end
